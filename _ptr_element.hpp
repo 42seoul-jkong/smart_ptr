89422d9b2e9bc8c04d2cda3ce04c7d06d476c767
jkong <jkong@student.42seoul.kr>
2022-11-29 04:23:33 +0900

    !?

--- _ptr_element.hpp
+++ _ptr_element.hpp
@@ -7,6 +7,68 @@
 
 namespace ft
 {
+    template <typename T, T Value>
+    struct _ptr_integral_constant
+    {
+        typedef T value_type;
+        typedef _ptr_integral_constant<T, Value> type;
+        static const T value = Value;
+
+        operator T() const { return Value; }
+    };
+
+    typedef _ptr_integral_constant<bool, true> _ptr_true_type;
+    typedef _ptr_integral_constant<bool, false> _ptr_false_type;
+
+    template <bool B, typename T = void>
+    struct _ptr_enable_if
+    {
+        typedef T type;
+    };
+
+    template <typename T>
+    struct _ptr_enable_if<false, T>
+    {
+    };
+
+    template <typename TSource, typename TDest>
+    struct _ptr_convertible
+    {
+        typedef char (&yes)[1];
+        typedef char (&no)[2];
+
+        static yes f(TDest*);
+        static no f(...);
+
+        static const bool value = sizeof((f)(static_cast<TSource*>(0))) == sizeof(yes);
+    };
+
+    template <typename TSource, typename TDest>
+    struct _ptr_convertible<TSource, TDest[]>
+    {
+        static const bool value = false;
+    };
+
+    template <typename TSource, typename TDest>
+    struct _ptr_convertible<TSource[], TDest[]>
+    {
+        static const bool value = _ptr_convertible<TSource[1], TDest[1]>::value;
+    };
+
+    template <typename TSource, std::size_t N, typename TDest>
+    struct _ptr_convertible<TSource[N], TDest[]>
+    {
+        static const bool value = _ptr_convertible<TSource[1], TDest[1]>::value;
+    };
+
+    ///
+
+    template <typename TSource, typename TDest>
+    inline void _ptr_assert_convertible() throw()
+    {
+        (void)sizeof(char[_ptr_convertible<TSource, TDest>::value ? 1 : -1]);
+    }
+
     template <typename T>
     struct _ptr_element_type
     {
@@ -106,66 +168,70 @@ namespace ft
     template <typename T>
     struct _ptr_array_extent
     {
-        enum
-        {
-            value = 0
-        };
+        static const std::size_t value = 0;
     };
 
     template <typename T, std::size_t N>
     struct _ptr_array_extent<T[N]>
     {
-        enum
-        {
-            value = N
-        };
+        static const std::size_t value = N;
     };
 
-    template <typename TSource, typename TDest>
-    struct _ptr_convertible
+    template <typename T>
+    struct _ptr_is_unbounded_array : _ptr_false_type
     {
-        typedef char (&yes)[1];
-        typedef char (&no)[2];
-
-        static yes f(TDest*);
-        static no f(...);
-
-        enum
-        {
-            value = sizeof((f)(static_cast<TSource*>(0))) == sizeof(yes)
-        };
     };
 
-    template <typename TSource, typename TDest>
-    struct _ptr_convertible<TSource, TDest[]>
+    template <typename T>
+    struct _ptr_is_unbounded_array<T[]> : _ptr_true_type
     {
-        enum
-        {
-            value = false
-        };
     };
 
-    template <typename TSource, typename TDest>
-    struct _ptr_convertible<TSource[], TDest[]>
+    template <typename T>
+    struct _ptr_is_unbounded_array<const T[]> : _ptr_true_type
     {
-        enum
-        {
-            value = _ptr_convertible<TSource[1], TDest[1]>::value
-        };
     };
 
-    template <typename TSource, std::size_t N, typename TDest>
-    struct _ptr_convertible<TSource[N], TDest[]>
+    template <typename T>
+    struct _ptr_is_unbounded_array<volatile T[]> : _ptr_true_type
     {
-        enum
-        {
-            value = _ptr_convertible<TSource[1], TDest[1]>::value
-        };
     };
 
-    template <typename TSource, typename TDest>
-    inline void _ptr_assert_convertible() throw()
+    template <typename T>
+    struct _ptr_is_unbounded_array<const volatile T[]> : _ptr_true_type
     {
-        (void)sizeof(char[_ptr_convertible<TSource, TDest>::value ? 1 : -1]);
-    }
+    };
+
+    template <typename T>
+    struct _ptr_is_bounded_array : _ptr_false_type
+    {
+    };
+    template <typename T, std::size_t N>
+    struct _ptr_is_bounded_array<T[N]> : _ptr_true_type
+    {
+    };
+
+    template <typename T, std::size_t N>
+    struct _ptr_is_bounded_array<const T[N]> : _ptr_true_type
+    {
+    };
+
+    template <typename T, std::size_t N>
+    struct _ptr_is_bounded_array<volatile T[N]> : _ptr_true_type
+    {
+    };
+
+    template <typename T, std::size_t N>
+    struct _ptr_is_bounded_array<const volatile T[N]> : _ptr_true_type
+    {
+    };
+
+    template <typename T>
+    struct _ptr_is_not_array
+        : _ptr_integral_constant<
+              bool,
+              !_ptr_is_unbounded_array<T>::value &&
+                  !_ptr_is_bounded_array<T>::value>
+    {
+    };
 }
