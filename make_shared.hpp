0b8b23267ad454c3e52ef66cbc10cb3670356b73
jkong <jkong@student.42seoul.kr>
2022-12-10 17:47:47 +0900

    생성자 호출 노가다

--- make_shared.hpp
+++ make_shared.hpp
@@ -146,20 +146,64 @@ namespace ft
         return ft::shared_ptr<T>(_internal::internal_tag(), _internal::deleter_storage<T, TAlloc>(T(), a));
     }
 
+    template <typename T, typename TAlloc, typename A1>
+    typename _internal::enable_if<!_internal::is_array<T>::value, ft::shared_ptr<T> >::type allocate_shared(const TAlloc& a, const A1& a1)
+    {
+        return ft::shared_ptr<T>(_internal::internal_tag(), _internal::deleter_storage<T, TAlloc>(T(a1), a));
+    }
+
+    template <typename T, typename TAlloc, typename A1, typename A2>
+    typename _internal::enable_if<!_internal::is_array<T>::value, ft::shared_ptr<T> >::type allocate_shared(const TAlloc& a, const A1& a1, const A2& a2)
+    {
+        return ft::shared_ptr<T>(_internal::internal_tag(), _internal::deleter_storage<T, TAlloc>(T(a1, a2), a));
+    }
+
+    template <typename T, typename TAlloc, typename A1, typename A2, typename A3>
+    typename _internal::enable_if<!_internal::is_array<T>::value, ft::shared_ptr<T> >::type allocate_shared(const TAlloc& a, const A1& a1, const A2& a2, const A3& a3)
+    {
+        return ft::shared_ptr<T>(_internal::internal_tag(), _internal::deleter_storage<T, TAlloc>(T(a1, a2, a3), a));
+    }
+
+    template <typename T, typename TAlloc, typename A1, typename A2, typename A3, typename A4>
+    typename _internal::enable_if<!_internal::is_array<T>::value, ft::shared_ptr<T> >::type allocate_shared(const TAlloc& a, const A1& a1, const A2& a2, const A3& a3, const A4& a4)
+    {
+        return ft::shared_ptr<T>(_internal::internal_tag(), _internal::deleter_storage<T, TAlloc>(T(a1, a2, a3, a4), a));
+    }
+
+    template <typename T, typename TAlloc, typename A1, typename A2, typename A3, typename A4, typename A5>
+    typename _internal::enable_if<!_internal::is_array<T>::value, ft::shared_ptr<T> >::type allocate_shared(const TAlloc& a, const A1& a1, const A2& a2, const A3& a3, const A4& a4, const A5& a5)
+    {
+        return ft::shared_ptr<T>(_internal::internal_tag(), _internal::deleter_storage<T, TAlloc>(T(a1, a2, a3, a4, a5), a));
+    }
+
+    template <typename T, typename TAlloc, typename A1, typename A2, typename A3, typename A4, typename A5, typename A6>
+    typename _internal::enable_if<!_internal::is_array<T>::value, ft::shared_ptr<T> >::type allocate_shared(const TAlloc& a, const A1& a1, const A2& a2, const A3& a3, const A4& a4, const A5& a5, const A6& a6)
+    {
+        return ft::shared_ptr<T>(_internal::internal_tag(), _internal::deleter_storage<T, TAlloc>(T(a1, a2, a3, a4, a5, a6), a));
+    }
+
+    template <typename T, typename TAlloc, typename A1, typename A2, typename A3, typename A4, typename A5, typename A6, typename A7>
+    typename _internal::enable_if<!_internal::is_array<T>::value, ft::shared_ptr<T> >::type allocate_shared(const TAlloc& a, const A1& a1, const A2& a2, const A3& a3, const A4& a4, const A5& a5, const A6& a6, const A7& a7)
+    {
+        return ft::shared_ptr<T>(_internal::internal_tag(), _internal::deleter_storage<T, TAlloc>(T(a1, a2, a3, a4, a5, a6, a7), a));
+    }
+
+    template <typename T, typename TAlloc, typename A1, typename A2, typename A3, typename A4, typename A5, typename A6, typename A7, typename A8>
+    typename _internal::enable_if<!_internal::is_array<T>::value, ft::shared_ptr<T> >::type allocate_shared(const TAlloc& a, const A1& a1, const A2& a2, const A3& a3, const A4& a4, const A5& a5, const A6& a6, const A7& a7, const A8& a8)
+    {
+        return ft::shared_ptr<T>(_internal::internal_tag(), _internal::deleter_storage<T, TAlloc>(T(a1, a2, a3, a4, a5, a6, a7, a8), a));
+    }
+
+    template <typename T, typename TAlloc, typename A1, typename A2, typename A3, typename A4, typename A5, typename A6, typename A7, typename A8, typename A9>
+    typename _internal::enable_if<!_internal::is_array<T>::value, ft::shared_ptr<T> >::type allocate_shared(const TAlloc& a, const A1& a1, const A2& a2, const A3& a3, const A4& a4, const A5& a5, const A6& a6, const A7& a7, const A8& a8, const A9& a9)
+    {
+        return ft::shared_ptr<T>(_internal::internal_tag(), _internal::deleter_storage<T, TAlloc>(T(a1, a2, a3, a4, a5, a6, a7, a8, a9), a));
+    }
+
     template <typename T, typename TAlloc>
     typename _internal::enable_if<_internal::is_bounded_array<T>::value, ft::shared_ptr<T> >::type allocate_shared(const TAlloc& a)
     {
         return ft::shared_ptr<T>(_internal::internal_tag(), _internal::deleter_storage<T, TAlloc>(a));
-    }
-
-    template <typename T, typename TAlloc>
-    typename _internal::enable_if<_internal::is_unbounded_array<T>::value, ft::shared_ptr<T> >::type allocate_shared(const TAlloc& a, std::size_t n)
-    {
-        typedef typename TAlloc::template rebind<typename _internal::element_type<T>::type>::other alloc_type;
-        _internal::allocate_guard<alloc_type> alloc(a, n);
-        ft::shared_ptr<T> result = ft::shared_ptr<T>(alloc.get(), _internal::allocator_delete<T, alloc_type>(a, n), a);
-        alloc.reset();
-        return result;
     }
 
     template <typename T, typename TAlloc>
@@ -171,6 +215,16 @@ namespace ft
         {
             result[--n] = def;
         }
+        return result;
+    }
+
+    template <typename T, typename TAlloc>
+    typename _internal::enable_if<_internal::is_unbounded_array<T>::value, ft::shared_ptr<T> >::type allocate_shared(const TAlloc& a, std::size_t n)
+    {
+        typedef typename TAlloc::template rebind<typename _internal::element_type<T>::type>::other alloc_type;
+        _internal::allocate_guard<alloc_type> alloc(a, n);
+        ft::shared_ptr<T> result = ft::shared_ptr<T>(alloc.get(), _internal::allocator_delete<T, alloc_type>(a, n), a);
+        alloc.reset();
         return result;
     }
 
@@ -188,15 +242,60 @@ namespace ft
     template <typename T>
     ft::shared_ptr<T> make_shared()
     {
-        typedef std::allocator<T> allocate_type;
-        return allocate_shared<T, allocate_type>(allocate_type());
+        return allocate_shared<T>(std::allocator<T>());
     }
 
-    // TODO: Forward arguments and use internal constructor
+    template <typename T, typename A1>
+    ft::shared_ptr<T> make_shared(const A1& a1)
+    {
+        return allocate_shared<T>(std::allocator<T>(), a1);
+    }
 
-    // template <typename T, typename TAlloc, typename A1, typename A2, typename A3, typename A4, typename A5, typename A6, typename A7, typename A8, typename A9>
-    // ft::shared_ptr<T> allocate_shared(const TAlloc& a, const A1& a1, const A2& a2, const A3& a3, const A4& a4, const A5& a5, const A6& a6, const A7& a7, const A8& a8, const A9& a9);
+    template <typename T, typename A1, typename A2>
+    ft::shared_ptr<T> make_shared(const A1& a1, const A2& a2)
+    {
+        return allocate_shared<T>(std::allocator<T>(), a1, a2);
+    }
 
-    // template <typename T, typename A1, typename A2, typename A3, typename A4, typename A5, typename A6, typename A7, typename A8, typename A9>
-    // ft::shared_ptr<T> make_shared(const A1& a1, const A2& a2, const A3& a3, const A4& a4, const A5& a5, const A6& a6, const A7& a7, const A8& a8, const A9& a9);
+    template <typename T, typename A1, typename A2, typename A3>
+    ft::shared_ptr<T> make_shared(const A1& a1, const A2& a2, const A3& a3)
+    {
+        return allocate_shared<T>(std::allocator<T>(), a1, a2, a3);
+    }
+
+    template <typename T, typename A1, typename A2, typename A3, typename A4>
+    ft::shared_ptr<T> make_shared(const A1& a1, const A2& a2, const A3& a3, const A4& a4)
+    {
+        return allocate_shared<T>(std::allocator<T>(), a1, a2, a3, a4);
+    }
+
+    template <typename T, typename A1, typename A2, typename A3, typename A4, typename A5>
+    ft::shared_ptr<T> make_shared(const A1& a1, const A2& a2, const A3& a3, const A4& a4, const A5& a5)
+    {
+        return allocate_shared<T>(std::allocator<T>(), a1, a2, a3, a4, a5);
+    }
+
+    template <typename T, typename A1, typename A2, typename A3, typename A4, typename A5, typename A6>
+    ft::shared_ptr<T> make_shared(const A1& a1, const A2& a2, const A3& a3, const A4& a4, const A5& a5, const A6& a6)
+    {
+        return allocate_shared<T>(std::allocator<T>(), a1, a2, a3, a4, a5, a6);
+    }
+
+    template <typename T, typename A1, typename A2, typename A3, typename A4, typename A5, typename A6, typename A7>
+    ft::shared_ptr<T> make_shared(const A1& a1, const A2& a2, const A3& a3, const A4& a4, const A5& a5, const A6& a6, const A7& a7)
+    {
+        return allocate_shared<T>(std::allocator<T>(), a1, a2, a3, a4, a5, a6, a7);
+    }
+
+    template <typename T, typename A1, typename A2, typename A3, typename A4, typename A5, typename A6, typename A7, typename A8>
+    ft::shared_ptr<T> make_shared(const A1& a1, const A2& a2, const A3& a3, const A4& a4, const A5& a5, const A6& a6, const A7& a7, const A8& a8)
+    {
+        return allocate_shared<T>(std::allocator<T>(), a1, a2, a3, a4, a5, a6, a7, a8);
+    }
+
+    template <typename T, typename A1, typename A2, typename A3, typename A4, typename A5, typename A6, typename A7, typename A8, typename A9>
+    ft::shared_ptr<T> make_shared(const A1& a1, const A2& a2, const A3& a3, const A4& a4, const A5& a5, const A6& a6, const A7& a7, const A8& a8, const A9& a9)
+    {
+        return allocate_shared<T>(std::allocator<T>(), a1, a2, a3, a4, a5, a6, a7, a8, a9);
+    }
 }
